5d03332cf68b05b8be904f2c1b578daaf84bcc66
ActualGEO <gbadamosioluwatobi10@gmail.com>
2025-09-14 18:03:20 +0100

    basic operations file added

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result1 = number1 + number2
+print(f"Addition of {number1} and {number2} is {result1}")
+result2 = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result2}")
+result3 = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {result3}")

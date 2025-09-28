2f270fb3edafe571ca5163e3f1bcb08413c2a1da
ActualGEO <gbadamosioluwatobi10@gmail.com>
2025-09-28 23:12:38 +0100

    Corrrected my speeling

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,24 +1,24 @@
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-FAHRENHEIT_TO_CELSUIS_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
-def convert_to_celsuis(fahrenheit):
-    """convert Fahrenheit to Celsuis."""
-    return(fahrenheit - 32) * FAHRENHEIT_TO_CELSUIS_FACTOR
+def convert_to_celsius(fahrenheit):
+    """convert Fahrenheit to Celsius."""
+    return(fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 
-def convert_to_fahrenheit(celsuis):
+def convert_to_fahrenheit(celsius):
     """convert to fahrenheit."""
-    return (celsuis * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 
 def main():
     temp = input("Enter the temperature to convert: ")
-    question = input("Is this temperaturein celsuis or fahrenheit? (C/F): ")
+    question = input("Is this temperaturein celsius or fahrenheit? (C/F): ")
     if question == "C":
         converted = convert_to_fahrenheit(float(temp))
         print(f"{temp}°C is {converted}°F")
     elif question == "F":
-        converted = convert_to_celsuis(float(temp))
+        converted = convert_to_celsius(float(temp))
         print(f"{temp}°F is {converted:.2f}°C")
     else:
         print("Invalid input. Please enter 'C' for Celsuis or 'F' for fahrenheit")

ecf6ec79e9e303178f44215698545dc4e445783c
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-17 21:04:54 +0100

    Updated

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+from datetime import datetime,timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+def calculate_future_date():
+    enter_future_date = int(input("Enter the number of days to add to the current date: "))
+    future_date = timedelta(days=enter_future_date)
+    future_date.strftime
+
+    print("Future date:",future_date + datetime.now().date())
+
+display_current_datetime()
+calculate_future_date()
---
+++ programming_paradigm/tester.py
@@ -0,0 +1,5 @@
+import robust_division_calculator
+
+number = robust_division_calculator.safe_divide(5,1)
+
+print(number)
